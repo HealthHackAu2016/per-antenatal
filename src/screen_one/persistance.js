eba01b009c39ceb2dce1cb1c858a5cefc46e7f70
thinqdeliver <sameerchauhan@outlook.com>
2016-10-16 00:05:55 +0800

    took out persistence logic and added name attri to get persistence to work

---
+++ src/screen_one/persistance.js
@@ -0,0 +1,38 @@
+ var fs = require('fs'); // Load the File System to execute our common tasks (CRUD)
+
+
+  $.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
+ 
+$(function() {
+    $('form').submit(function() {
+        //console.log(JSON.stringify($('form').serializeObject()));
+		//TODO validation
+		var fileName = "screen_one"; //TODO folder 
+		var content = JSON.stringify($('form').serializeObject());
+		 fs.writeFile(fileName, content, function (err) {
+			 
+           if(err){
+               alert("An error ocurred creating the file "+ err.message)
+           }
+                        
+           alert("The file has been succesfully saved");
+       });
+
+        return false;
+    });
+});
